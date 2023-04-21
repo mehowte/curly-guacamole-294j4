0349f4050579c185a7e64783890c95df191a3cc5
Michał Taszycki <mtaszycki@gmail.com>
2023-04-21 14:49:30 +0200

    Add minimal frontend interface

--- app/frontend/components/App.tsx
+++ app/frontend/components/App.tsx
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
 export function App() {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Hello React</h1>
-      <p>clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <h1>Ask my book</h1>
+      <p>
+        This is an experiment in using AI to make my book's content more
+        accessible. Ask a question and AI'll answer it in real-time:
+      </p>
+      <form>
+        <textarea
+          defaultValue={"What is a minimalist entrepreneur?"}
+          name="question"
+          id="question"
+          cols={30}
+          rows={3}
+        />
+      </form>
+      <button type="button">Ask question</button>
     </>
   );
 }
