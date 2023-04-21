9157fb150f38281198b30075aca8f190bb50ebb1
Michał Taszycki <mtaszycki@gmail.com>
2023-04-21 15:34:34 +0200

    Return a dummy error when wrong question is asked
    this allows us to test error handling on the frontend

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,5 +1,9 @@
 class Api::QuestionsController < ApiController
   def create
+    if params[:question] == "What?"
+      render json: { error: "What what?" }, status: :unprocessable_entity
+      return
+    end
     question = { answer: "A minimalist entrepreneur is someone who builds a business according to eight principles, the first of which is prioritizing profitability over growth." }
     render json: question, status: :created
   end
