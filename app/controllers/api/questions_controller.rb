563809efb0a512b3c6c160da2d6b83af5313de34
Michał Taszycki <mtaszycki@gmail.com>
2023-04-22 12:39:51 +0200

    Simulate AI answering questions.
    There are two questions answered by github copilot.
    
    If we don't find the answer we respond with 'I don't know (...)'

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -4,7 +4,20 @@ class Api::QuestionsController < ApiController
       render json: { error: "Question cannot be empty" }, status: :unprocessable_entity
       return
     end
-    question = { answer: "A minimalist entrepreneur is someone who builds a business according to eight principles, the first of which is prioritizing profitability over growth." }
+    question_asked = Question.normalize_question(params[:question])
+    answer = generate_answer(question_asked)
+    question = { answer: answer }
     render json: question, status: :created
   end
+end
+
+SAMPLE_QUESTIONS = {
+  "What is a minimalist entrepreneur?" => "A minimalist entrepreneur is someone who builds a business according to eight principles, the first of which is prioritizing profitability over growth.",
+  "Who is Sahil Lavingia?" => "Sahil Lavingia is the founder of Gumroad, a minimalist entrepreneur, and the author of Minimalist Entrepreneurship.",
+}
+
+def generate_answer(question_asked)
+  answer = SAMPLE_QUESTIONS[question_asked] 
+  return "I don't know the answer to that question." if answer.blank?
+  answer
 end
