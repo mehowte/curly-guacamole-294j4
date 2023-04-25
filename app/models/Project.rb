81ef8a87f56a5cabfc53fd7642dbdc43bc382349
Michał Taszycki <mtaszycki@gmail.com>
2023-04-25 13:34:02 +0200

    Add Project model
    it reads settings from appropriate yaml file.

---
+++ app/models/Project.rb
@@ -0,0 +1,39 @@
+class Project
+    def build
+        new(ENV["PROJECT_NAME"])
+    end
+
+    def initialize(name)
+        @name = name
+        @config = YAML.load_file(file_path("#{name}.yaml"))
+    end
+
+    def sample_questions
+        @config["sample_questions"]
+    end
+
+    def questions_and_answers
+        @config["questions_and_answers"]
+    end
+
+    def prompt_header
+        @config["prompt_header"]
+    end
+
+    def context_header
+        @config["context_header"]
+    end
+
+    def fragments_file_path
+        file_path("#{@name}.pdf.pages.csv")
+    end
+
+    def embeddings_file_path
+        file_path("#{@name}.pdf.embeddings.csv")
+    end
+
+    private
+    def file_path(file_name)
+        Rails.root.join("static_files", file_name)
+    end
+end
