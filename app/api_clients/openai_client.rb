caa24bd691d3b055faf4ad8f19197e7d8d7e803f
Michał Taszycki <mtaszycki@gmail.com>
2023-04-25 23:00:25 +0200

    Expose chat endpoint through OpenaiClient

--- app/api_clients/openai_client.rb
+++ app/api_clients/openai_client.rb
@@ -3,7 +3,9 @@ class OpenaiClient
     EMBEDDINGS_MODEL = "text-embedding-ada-002"
     MAX_EMBEDDING_TOKENS = 8191
     MAX_EMBEDDING_DIMENSIONS = 1536
+    CHAT_MODELS = ["gpt-3.5-turbo"]
 
+    VALID_CHAT_AND_COMPLETION_MODELS = [*CHAT_MODELS, COMPLETIONS_MODEL]
     def self.build()
         new(ENV["OPENAI_API_KEY"])
     end
@@ -29,6 +31,16 @@ class OpenaiClient
                 input: text
             })
         result["data"][0]["embedding"]
+    end
+
+    def get_chat(messages, model)
+        result = @client.chat(
+            parameters: {
+                model: model,
+                messages: messages,
+                temperature: 0.0
+            })
+        result.dig("choices", 0, "message", "content")
     end
 
     def self.token_encoder_for_embeddings
